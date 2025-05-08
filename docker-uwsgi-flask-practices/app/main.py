4d570e0a6db45bce0a49e4308036d944c03cd1e6
Kin Yuunan <yuunan.kin@gmail.com>
2025-05-08 15:32:42 +0900

    access secret key

--- docker-uwsgi-flask-practices/app/main.py
+++ docker-uwsgi-flask-practices/app/main.py
@@ -1,7 +1,8 @@
+import os
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/api")
 def index():
-    return jsonify(message="Hello from Flask + uWSGI + Nginx!")
+    return jsonify(message="Hello from Flask + uWSGI + Nginx!", key=os.getenv("API_KEY"))
