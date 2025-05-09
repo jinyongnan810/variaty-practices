1897553061cfaeaaf70b6e8f863bdb369f5dfe9d
Kin Yuunan <yuunan.kin@gmail.com>
2025-05-09 10:09:58 +0900

    single container

--- docker-uwsgi-flask-practices/app/main.py
+++ docker-uwsgi-flask-practices/app/main.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/api")
+@app.route("/api", methods=["GET"])
 def index():
     return jsonify(message="Hello from Flask + uWSGI + Nginx!", key=os.getenv("API_KEY"))
 
