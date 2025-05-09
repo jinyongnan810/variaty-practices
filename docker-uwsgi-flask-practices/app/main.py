fd2d64571e09ca6ccae3ac792b7516d518c1ad12
Kin Yuunan <yuunan.kin@gmail.com>
2025-05-09 10:12:12 +0900

    test other api routes

--- docker-uwsgi-flask-practices/app/main.py
+++ docker-uwsgi-flask-practices/app/main.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return jsonify(message="Hello from Flask + uWSGI + Nginx!", key=os.getenv("API_KEY"))
 
+@app.route("/some-other-api", methods=["GET"])
+def some_other():
+    return jsonify(message="Hello from Flask + uWSGI + Nginx!(some-other-api)", key=os.getenv("API_KEY"))
+
 if __name__ == "__main__":
     app.run()
