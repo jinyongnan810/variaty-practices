f934fd1e3b560ec0bc2d4da142c56f9055865942
Kin Yuunan <yuunan.kin@gmail.com>
2025-07-13 15:41:27 +0900

    add an api

--- django_basics/django_basics/urls.py
+++ django_basics/django_basics/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from .views import book_list
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/books/', book_list, name='book-list'),
 ]
