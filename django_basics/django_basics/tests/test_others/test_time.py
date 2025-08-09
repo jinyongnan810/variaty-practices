9b195f3d8119deacd70fdd463a3749aef82c8234
Kin Yuunan <yuunan.kin@gmail.com>
2025-08-09 09:57:48 +0900

    test with freezed time

---
+++ django_basics/django_basics/tests/test_others/test_time.py
@@ -0,0 +1,13 @@
+from datetime import datetime, timedelta, timezone
+import time
+import pytest
+
+JST = timezone(timedelta(hours=+9), "JST")
+
+
+@pytest.mark.freeze_time(datetime(2025, 1, 1, 12, 0, 0, tzinfo=JST))
+class TestTime:
+    def test_time(self):
+        assert datetime.now(tz=JST) == datetime(2025, 1, 1, 12, 0, 0, tzinfo=JST)
+        assert datetime.now() == datetime(2025, 1, 1, 3, 0, 0)
+        assert int(time.time()) == 1735700400
