d5038f74643b2ca0402ab4d7b0bf43d9a6516706
Kin Yuunan <yuunan.kin@gmail.com>
2025-09-15 15:08:25 +0900

    basic usages

--- dotenvx-practice/myproject/views/env_test.py
+++ dotenvx-practice/myproject/views/env_test.py
@@ -5,4 +5,5 @@ from decouple import config
 def test_env(request):
     env1 = config("ENV_KEY1", default="default_value1")
     env2 = config("ENV_KEY2", default="default_value2")
-    return JsonResponse({"env1": env1, "env2": env2})
+    env3 = config("ENV_KEY3", default="default_value3")
+    return JsonResponse({"env1": env1, "env2": env2, "env3": env3})
